067dd8c456c9c6a82cf35a77f1b03dbab4c0ec46
Awindsr <84223604+awindsr@users.noreply.github.com>
2023-11-01 19:57:38 +1030

    Create RecursiveFibonacci.java

---
+++ RecursiveFibonacci.java
@@ -0,0 +1,27 @@
+package javaremed;
+import java.util.Scanner;
+
+public class FibonacciRecursive {
+
+  public static int fibonacci(int n) {
+    if (n <= 1) {
+      return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter the value of n for the Fibonacci series: ");
+    
+    int n = scanner.nextInt();
+
+    System.out.println("Fibonacci series up to " + n + " terms:");
+    for (int i = 0; i < n; i++) {
+      System.out.print(fibonacci(i) + " ");
+    }
+
+    scanner.close();
+  }
+}
+
